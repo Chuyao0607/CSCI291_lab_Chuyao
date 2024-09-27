0b9f21b1dba89a3bff55192d3a1c0a09397fed69
Chuyao0607 <cz081@uowmail.edu.au>
2024-09-27 12:30:01 +0400

    Add files via upload

---
+++ lab2CSCI291.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+int main (void)
+{
+    printf(" Order coffee(1) |Admin mode(2) |End the application execution(3)\n ");
+    int i;
+    while(1)
+    {
+        printf("Enter your option(digits): ");
+        scanf("%d",&i);
+
+        if (i<2)
+
+        printf("Number; Coffee type; Price(AED)\n");
+        printf("1; Epresso; 3.5\n\n");
+        printf("2; Cappuccino; 4.5\n\n\n");
+        printf("3; Mocha; 5.5\n\n\n\n");
+        scanf("%s%s%s",&Expresso,&Cappucino,&Mocha);
+
+        int option
+        printf("Enter your option(1,2,3): ");
+        scanf("%d",&option);
+            if(option==1);
+            printf() 
+        
+
+
+        if (i==2)
+
+
+        if(i>3)
+    }
+
+return 0;
+}
