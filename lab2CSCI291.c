85bd78b395bb9d69ca0d4762b357accb46033088
Chuyao0607 <cz081@uowmail.edu.au>
2024-09-29 19:38:13 +0400

    Update lab2CSCI291.c

--- lab2CSCI291.c
+++ lab2CSCI291.c
@@ -1,5 +1,43 @@
 #include<stdio.h>
 #include<math.h>
+
+/*Coffee Beans(grams)*/
+#define Espresso_bean 8
+#define Cappuccino_bean 8
+#define Mocha_bean 8
+
+/*Water(milliliters)*/
+#define Espresso_water 30
+#define Cappuccino_water 30
+#define Mocha_water 30
+
+/*Milk(milliliters)*/
+#define Cappuccino_milk 70
+#define Mocha_milk 160
+
+/*Chocolate Syrup(milliliters)*/
+#define Espresso_chocolate 0
+#define Cappuccino_chocolate 0
+#define Mocha_chocolate 30
+
+/*Price(AED)*/
+#define Espresso_price 3.5
+#define Capppuccino_price 4.5
+#define Mocha_price 5.5
+
+/*total materials*/
+#define bean 300
+#define water 600
+#define milk 1000
+#define chocolate 550
+
+/*low threshold*/
+#define bean_lowthreshold 9
+#define water_threshold 40
+#define milk_threshold 75
+#define chocolate_threshold 35
+
+
 int main (void)
 {
     printf(" Order coffee(1) |Admin mode(2) |End the application execution(3)\n ");
@@ -25,10 +63,10 @@ int main (void)
         
 
 
-        if (i==2)
+     if (i==2)
 
 
-        if(i>3)
+    if(i>2)
     }
 
 return 0;
