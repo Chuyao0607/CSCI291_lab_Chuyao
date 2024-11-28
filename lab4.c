a6c2c917e6f90d4b8d4bbdb21b8487424feca01a
Chuyao0607 <cz081@uowmail.edu.au>
2024-11-29 01:22:41 +0400

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -61,22 +61,56 @@ int main() {
     return 0;
 }
 question3：
-a.
 #include <iostream>
 #include <iomanip>  // for std::setw, std::fixed, std::setprecision
 using namespace std;
-void print_real( float number, int fieldspace, int precision){
-cout << setw(fieldspace)
-              << fixed
-              << setprecision(precision)
-              << number
-              << endl;
-    
+
+// Template function for printing a number in fixed-point format with specified field width and precision
+template <typename T>
+void print_real(T number, int fieldspace, int precision) {
+    cout << setw(fieldspace)      // Set the field width
+         << fixed                 // Use fixed-point notation
+         << setprecision(precision)  // Set the number of decimal places
+         << number
+         << endl;  // Print the number with a newline
 }
+
+// Template function to update the values of m1 and m2
+template <typename T>
+void update_scale(T& m1, T& m2, T m3 = 10) {
+    T original_m1 = m1;
+    T original_m2 = m2;
+    m1 = (original_m1 + original_m2) * m3;  // Update m1
+    m2 = (original_m1 - original_m2) * m3;  // Update m2
+}
+
 int main() {
+    float m1, m2;
+
+    // Get input values for m1 and m2
+    cout << "Enter your m1 value: ";
+    cin >> m1;
+    cout << "Enter your m2 value: ";
+    cin >> m2;
+
     float number = 123.45678;
     int fieldspace = 10;
     int precision = 3;
-    print_real(number,fieldspace,precision);
+    float m3 = 10;
+
+    // Print the number in fixed-point notation
+    print_real(number, fieldspace, precision);
+
+    // Print m1 and m2 before the update
+    cout << "Before update_scale:" << endl;
+    cout << "m1 = " << m1 << ", m2 = " << m2 << endl;
+
+    // Update m1 and m2 using the update_scale function
+    update_scale(m1, m2, m3);
+
+    // Print m1 and m2 after the update
+    cout << "After update_scale:" << endl;
+    cout << "m1 = " << m1 << ", m2 = " << m2 << endl;
+
     return 0;
 }
